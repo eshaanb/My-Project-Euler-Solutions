9ed42dc1ef23741b7956b2c2e313afb16541abce
eshaanb <eshaan55@gmail.com>
2015-02-05 23:05:19 -0800

    prob114

---
+++ prob114.py
@@ -0,0 +1,11 @@
+blocks = []
+
+#for i in range(0, 50):
+#    blocks.append(0) #black
+
+for i in range(0, 7):
+    blocks.append(0) #black
+
+def addRed(index):
+    for block in blocks():
+        addRed(index)
